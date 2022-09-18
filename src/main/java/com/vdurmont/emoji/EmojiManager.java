57211bad31ed0074e7c5c6c508acbc6a5fcc5e31
Charles COHEN <charles@bodyguard.ai>
2022-09-18 14:46:52 +0200

    Fix bug

--- src/main/java/com/vdurmont/emoji/EmojiManager.java
+++ src/main/java/com/vdurmont/emoji/EmojiManager.java
@@ -37,7 +37,7 @@ public class EmojiManager {
       ALL_EMOJIS = emojis;
       ALL_EMOJIS.addAll(flagsAlternativesEmojis);
 
-      for (Emoji emoji : emojis) {
+      for (Emoji emoji : ALL_EMOJIS) {
         for (String tag : emoji.getTags()) {
           if (EMOJIS_BY_TAG.get(tag) == null) {
             EMOJIS_BY_TAG.put(tag, new HashSet<Emoji>());
@@ -49,7 +49,7 @@ public class EmojiManager {
         }
       }
 
-      EMOJI_TRIE = new EmojiTrie(emojis);
+      EMOJI_TRIE = new EmojiTrie(ALL_EMOJIS);
       Collections.sort(ALL_EMOJIS, new Comparator<Emoji>() {
         public int compare(Emoji e1, Emoji e2) {
           return e2.getUnicode().length() - e1.getUnicode().length();
